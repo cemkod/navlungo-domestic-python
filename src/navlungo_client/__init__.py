cd68ec182b557c1b867f5b49d03b92658562cf1f
Cem <jimqode@gmail.com>
2025-03-14 15:29:27 +0300

    bump version 2025.1006-alpha -> 2025.1007-alpha

--- src/navlungo_client/__init__.py
+++ src/navlungo_client/__init__.py
@@ -1,3 +1,3 @@
 from .client import NavlungoClient
 from .models import CreatePostRequest, Address, PostDetails, PostItem, CreateTokenRequest, SenderAddress, CreateAddressRequest, GetBarcodeRequest, CarrierId, BarcodeType
-__version__ = "2025.1006-alpha"
+__version__ = "2025.1007-alpha"
